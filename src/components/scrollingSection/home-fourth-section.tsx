d6b27aea7479713486258323c3478eff1c296abb
Olushola <ememobongumoren2@gmail.com>
2025-05-14 19:03:11 +0100

    update

--- src/components/scrollingSection/home-fourth-section.tsx
+++ src/components/scrollingSection/home-fourth-section.tsx
@@ -7,8 +7,8 @@ import lifestyle from "@/assets/svgs/portfolio_svgs/lifestyle.svg";
 import family from "@/assets/svgs/portfolio_svgs/family.svg";
 import videography from "@/assets/svgs/portfolio_svgs/videography.svg";
 import kids from "@/assets/images/kid-1.jpg";
-import pregnancy from "@/assets/svgs/portfolio_svgs/pregnancy.svg";
-import portrait from "@/assets/svgs/portfolio_svgs/portrait.svg";
+// import pregnancy from "@/assets/svgs/portfolio_svgs/pregnancy.svg";
+// import portrait from "@/assets/svgs/portfolio_svgs/portrait.svg";
 
 import Image from "next/image";
 import Button from "../button/button";

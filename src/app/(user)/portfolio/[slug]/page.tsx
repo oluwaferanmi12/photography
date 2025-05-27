7d46e1c87cb9ce7058213d3fad7b80c9980a219d
Olushola <ememobongumoren2@gmail.com>
2025-05-28 00:50:48 +0100

    update

--- src/app/(user)/portfolio/[slug]/page.tsx
+++ src/app/(user)/portfolio/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "next/navigation";
 import { PlanCards } from "@/components/plans-card/PlanCards";
 import { GalleryBox } from "@/components/galleryBox/gallery-box";
@@ -10,17 +10,39 @@ import { Banner } from "@/components/banner/banner";
 import Button from "@/components/button/button";
 import Image from "next/image";
 import rollingImage from "@/assets/svgs/rollingImage.svg";
-
-
+import { Modal } from "antd";
+import { ContactFrom } from "@/components/contact-form/contact-form";
+import bas_thanks from "@/assets/svgs/BAS_thanks_modal_icon.svg";
 
 const SinglePackages = () => {
   const { slug } = useParams();
+  const [isSessionFormModalOpen, setIsSessionFormModalOpen] = useState(false);
+  const [isThankYouModalOpen, setIsThankYouModalOpen] = useState(false);
+  const [selectedService, setSelectedService] = useState("");
+
+  const showModal = (service) => {
+    setSelectedService(service);
+    setIsSessionFormModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsSessionFormModalOpen(false);
+  };
+  // Submit form onClick
+  const handleReserveSpot = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsSessionFormModalOpen(false);
+    setIsThankYouModalOpen(true); // Open the thank you modal
+  };
 
   return (
     <div>
       <div className="px-5 pt-36  lg:px-14 3xl:!px-28">
         <div>
-          <p className="text-[#FBFAF7] font-playfair text-5xl lg:text-6xl"> {slug} </p>
+          <p className="text-[#FBFAF7] capitalize font-playfair text-5xl lg:text-6xl">
+            {" "}
+            {slug}{" "}
+          </p>
           <p className="text-[#C3C3C2] text-base lg:text-lg w-full md:!w-[90%] 3xl:!w-1/2">
             From polished headshots to soulful lifestyle captures, I craft
             images that do more than just “look good” . They speak volumes.
@@ -30,8 +52,18 @@ const SinglePackages = () => {
         </div>
         <div className="flex items-center justify-between mt-8">
           <div className="flex flex-wrap gap-y-5 gap-3">
-            <Button variant="filled"  text={`Book a ${slug} session`} />
-            <Button variant="bordered" size={'medium'}  textColor="text-white" borderVariant="light" text={`See pricing`} />
+            <Button
+              variant="filled"
+              onClick={() => showModal(`${slug}`)}
+              text={`Book a ${slug} session`}
+            />
+            <Button
+              variant="bordered"
+              size={"medium"}
+              textColor="text-white"
+              borderVariant="light"
+              text={`See pricing`}
+            />
           </div>
           <span className="hidden lg:flex">
             <Image
@@ -65,6 +97,67 @@ const SinglePackages = () => {
 
       <GalleryBox />
       <Footer />
+
+      {/* MODAL */}
+      <Modal
+        open={isSessionFormModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+        className="sessionForm_modal"
+        closeIcon={null}
+        width={800}
+        centered
+      >
+        <ContactFrom
+          onSubmit={handleReserveSpot}
+          selectedService={selectedService}
+          setSelectedService={setSelectedService}
+        />
+      </Modal>
+      {/* AFTER FORM FILLING MODAL */}
+      <Modal
+        open={isThankYouModalOpen}
+        onCancel={() => setIsThankYouModalOpen(false)}
+        footer={null}
+        className="sessionForm_modal"
+        closeIcon={null}
+        width={600}
+        centered
+      >
+        <div className="py-8 px-10 w-full">
+          <div className="flex justify-between items-start w-full">
+            <div className="flex flex-col gap-2">
+              <span>
+                <Image src={bas_thanks} alt="bas" />
+              </span>
+              <h3 className="font-playfair text-5xl text-white">
+                Thank you for your reservation
+              </h3>
+            </div>
+            <span>
+              <Image
+                src={rollingImage}
+                className="w-28 h-28"
+                alt="rollingImage"
+              />
+            </span>
+          </div>
+          <div className="flex flex-col gap-5 mt-10">
+            <p className="text-sm text-light-brown">
+              Thank you for reserving a spot with me, I will check my
+              availability and respond to your booking in less than 24hrs, If
+              confirmed you will receive a payment link and a confirmation email
+              from me, Please watch out for your junks and texts.
+            </p>
+            <div>
+              <p className="text-light-brown text-sm">yours sincerely</p>
+              <p className="text-[#5A5A50] text-sm font-valentiamo-reg">
+                shotbyportable
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };

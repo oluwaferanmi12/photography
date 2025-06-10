d7b9e0c7b0102134aa4f1891cdf1de759668f841
Olushola <ememobongumoren2@gmail.com>
2025-06-10 16:14:55 +0100

    update on weekly view

--- src/components/admin-components/sideNav/weekly-view/weekly-view.tsx
+++ src/components/admin-components/sideNav/weekly-view/weekly-view.tsx
@@ -1,19 +1,34 @@
-import React from "react";
-import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
+"use client";
+
+import { useNextCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import {
   createViewDay,
   createViewMonthAgenda,
   createViewMonthGrid,
   createViewWeek,
 } from "@schedule-x/calendar";
-import '@schedule-x/theme-default/dist/index.css'
+import { createEventsServicePlugin } from "@schedule-x/events-service";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
 
+import "@schedule-x/theme-default/dist/index.css";
+import { useEffect, useState } from "react";
 
 
 
 
 export const WeeklyCalendar = () => {
-  const calendar = useCalendarApp({
+  const eventsService = useState(() => createEventsServicePlugin())[0];
+
+    // Get today's date in YYYY-MM-DD format
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const todayDateString = `${year}-${month}-${day}`;
+
+
+
+  const calendar = useNextCalendarApp({
     views: [
       createViewDay(),
       createViewWeek(),
@@ -23,17 +38,24 @@ export const WeeklyCalendar = () => {
     events: [
       {
         id: "1",
-        title: "Event 1",
-        start: "2025-06-12 10:44",
-        end: "2025-06-12 12:00",
+        title: "Wedding - Premium Plan",
+        planType: "Premium",
+        start: "2025-06-12 00:44",
+        end: "2025-06-12 02:00",
       },
     ],
-    selectedDate: "2025-06-09"
-    // plugins: [eventsService],
+    selectedDate: todayDateString,
+    plugins: [
+      eventsService,
+      createEventModalPlugin()
+    ],
+    callbacks: {
+      onRender: () => {
+        // get all events
+        eventsService.getAll();
+      },
+    },
   });
-
-
-
 
   return (
     <div>
